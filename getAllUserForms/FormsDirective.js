9696fed0d4ff74861f81efb2c93f3d062b465c9c
bhaskar415 <bhaskar_415@yahoo.com>
2015-03-10 23:54:01 -0400

    v1

--- getAllUserForms/FormsDirective.js
+++ getAllUserForms/FormsDirective.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('Forms-Directive', ['Forms-Controller']);
 
-myApp.directive('userForms', function(){  
+myApp.directive('userFormsNotUsed', function(){  
   return {
     restrict: 'EA', 
 	scope: { url: '@', // if it is a object substitution
@@ -11,7 +11,7 @@ myApp.directive('userForms', function(){
   };
 });
 
-myApp.directive('userFormsNotUsed', function(){  
+myApp.directive('userForms', function(){  
 var htmlTemplate = "<div>"+
 						"<button ng-click='getAllUserFroms()' class='btn btn-info btn-xs'><span class='glyphicon glyphicon-play'></span>All Forms</button>"+
 							"<button type='button' class='btn btn-success btn-xs' ng-click='addFormShowView()'><span class='glyphicon glyphicon-plus'></span>Form</button><br><br>"+
