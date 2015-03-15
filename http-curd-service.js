d72db8316e402723e9968573489368cf3fee346b
bhaskar415 <bhaskar_415@yahoo.com>
2015-03-15 13:50:08 -0400

    V1

--- http-curd-service.js
+++ http-curd-service.js
@@ -4,10 +4,11 @@ myApp.config(function($httpProvider) {
     //Enable cross domain calls
     $httpProvider.defaults.useXDomain = true;
      $httpProvider.defaults.withCredentials = true;
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
     //Remove the header containing XMLHttpRequest used to identify ajax call 
     //that would prevent CORS from working
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
 
