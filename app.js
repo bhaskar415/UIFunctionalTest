6bc548342b19f9467ab7eb29a5064a9a15e71988
bhaskar415 <bhaskar_415@yahoo.com>
2015-03-15 11:43:57 -0400

    V1

--- app.js
+++ app.js
@@ -12,16 +12,10 @@ myApp.config(function($routeProvider, $httpProvider) {
 	}).otherwise('/');
    
    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-    $httpProvider.defaults.useXDomain = true;
-     $httpProvider.defaults.withCredentials = true;
-
-    //Remove the header containing XMLHttpRequest used to identify ajax call 
-    //that would prevent CORS from working
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-
+  
   })
 
-myApp.controller('layoutCtrl', function($scope, $rootScope, $http, $location) {
+myApp.controller('layoutCtrl', function($scope, $rootScope, $http, $location, $q) {
 
  // $scope.authenticated = true;
   
@@ -36,7 +30,8 @@ myApp.controller('layoutCtrl', function($scope, $rootScope, $http, $location) {
 	
 
     console.log(credentials) ;
-    $http.get('http://192.168.1.18:8080/user', {headers : headers}).success(function(data) {
+	
+    $http.post('http://192.168.1.18:8080/user', {headers : headers}).success(function(data) {
      if (data.name) {
         $rootScope.authenticated = true;
       } else {
@@ -48,6 +43,16 @@ myApp.controller('layoutCtrl', function($scope, $rootScope, $http, $location) {
       $rootScope.authenticated = false;
       callback && callback();
     });
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
   }
 
@@ -95,6 +100,17 @@ myApp.controller('navigation', function($rootScope, $scope, $http, $location) {
       $rootScope.authenticated = false;
       callback && callback();
     });
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
   }
 
