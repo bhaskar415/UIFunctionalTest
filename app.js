c39163d343160bb8fb9dea14725013de3ac617c3
bhaskar415 <bhaskar_415@yahoo.com>
2015-03-15 23:45:31 -0400

    V1

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ myApp.config(function($routeProvider, $httpProvider) {
 		templateUrl : 'login.html',
 		controller : 'layoutCtrl3'
 	}).otherwise('/');
-  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+   
+     $httpProvider.defaults.useXDomain = true;
+    // $httpProvider.defaults.withCredentials = true;
+
+    //Remove the header containing XMLHttpRequest used to identify ajax call 
+    //that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+ 
+ $httpProvider.defaults.headers.common = {};
+  $httpProvider.defaults.headers.post = {};
+  $httpProvider.defaults.headers.put = {};
+  $httpProvider.defaults.headers.patch = {};
   })
 
 myApp.controller('layoutCtrl', function($scope, $rootScope, $http, $location, $q) {
