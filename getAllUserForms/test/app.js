2e2b279e44f1f7597bd151e25a3bfd566ced8628
bhaskar415 <bhaskar_415@yahoo.com>
2015-09-14 19:29:21 -0400

    Update app.js

--- getAllUserForms/test/app.js
+++ getAllUserForms/test/app.js
@@ -1,31 +1,26 @@
 var myApp = angular.module('Menu-Directive', []);
 myApp.directive('menuLink', function() {
-var htmlTemplate =  '   <md-button  '  + 
- '       ng-class="{'active' : isSelected()}"  '  + 
- '       ui-sref="{{section.state}}"  '  + 
- '       ng-click="focusSection()">  '  + 
- '     {{section.name}}  '  + 
- '     <span class="md-visually-hidden"  '  + 
- '       ng-if="isSelected()">  '  + 
- '       current page  '  + 
- '     </span>  '  + 
- '  </md-button>  ' ;   
+var htmlTemplate =   '     <md-card>  '  + 
+ '         <img ng-src="{{imagePath}}" class="md-card-image" alt="Washed Out">  '  + 
+ '         <md-card-content>  '  + 
+ '           <h2 class="md-title">Paracosm</h2>  '  + 
+ '           <p>  '  + 
+ '             The titles of Washed Out's breakthrough song and the first single from Paracosm share the  '  + 
+ '             two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...  '  + 
+ '           </p>  '  + 
+ '         </md-card-content>  '  + 
+ '         <div class="md-actions" layout="row" layout-align="end center">  '  + 
+ '           <md-button>Action 1</md-button>  '  + 
+ '           <md-button>Action 2</md-button>  '  + 
+ '         </div>  '  + 
+ '      </md-card>  ' ;  
    
   return {    
     restrict: 'E',
     template: htmlTemplate,
-    link: function(scope, element) {
+    link: function() {
    	
-      scope.isSelected = function() {
-        return controller.isSelected(scope.section);
-      };
-
-      scope.focusSection = function() {
-        // set flag to be used later when
-        // $locationChangeSuccess calls openPage()
-        controller.autoFocusContent = true;
-      };
-    }
+   
   };
 })
 	
