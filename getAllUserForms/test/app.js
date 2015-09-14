e0b50f3282e46da4fe96a7239f4560a8cf3beee5
bhaskar415 <bhaskar_415@yahoo.com>
2015-09-14 19:21:42 -0400

    Update app.js

--- getAllUserForms/test/app.js
+++ getAllUserForms/test/app.js
@@ -1,14 +1,33 @@
-// created service so that we can use this service for setting up app level scope 
-var myApp = angular.module('myApp', ['Forms-Directive']);
+var myApp = angular.module('Menu-Directive', []);
+myApp.directive('menuLink', function() {
+var htmlTemplate =  '   <md-button  '  + 
+ '       ng-class="{'active' : isSelected()}"  '  + 
+ '       ui-sref="{{section.state}}"  '  + 
+ '       ng-click="focusSection()">  '  + 
+ '     {{section.name}}  '  + 
+ '     <span class="md-visually-hidden"  '  + 
+ '       ng-if="isSelected()">  '  + 
+ '       current page  '  + 
+ '     </span>  '  + 
+ '  </md-button>  ' ;   
+   
+  return {    
+    restrict: 'E',
+    template: htmlTemplate,
+    link: function(scope, element) {
+   	
+      scope.isSelected = function() {
+        return controller.isSelected(scope.section);
+      };
 
-
-
-myApp.controller('layoutCtrl', function($scope) {
-
-
- });
-
-
+      scope.focusSection = function() {
+        // set flag to be used later when
+        // $locationChangeSuccess calls openPage()
+        controller.autoFocusContent = true;
+      };
+    }
+  };
+})
 	
 	
 
