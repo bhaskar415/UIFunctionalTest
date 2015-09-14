9fa7e1d17a947d82b05f6c99cec1ff4ffbd385e5
bhaskar415 <bhaskar_415@yahoo.com>
2015-09-14 19:55:03 -0400

    Update app.js

--- getAllUserForms/test/app.js
+++ getAllUserForms/test/app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('Menu-Directive', []);
-
+myApp.directive('menuLink', function(){  
+var htmlTemplate = "<div><h2>Paracosm</h2><div>";
+  return {
+    restrict: 'EA', 
+    template: htmlTemplate   
+  };
 	
 	
 
