e45792ebcc1d5ae129e43cdaa6100ceea3929ca3
bhaskar415 <bhaskar_415@yahoo.com>
2015-09-14 20:10:13 -0400

    Update app.js

--- getAllUserForms/test/app.js
+++ getAllUserForms/test/app.js
@@ -1,10 +1,33 @@
 var myApp = angular.module('Menu-Directive', [])
-.directive('mySharedScope', function () {
-    return {
-        template: 'Name'
-    };
-});
-	
+ .directive('menuLinkTemp', function() {
+var htmlTemplate =  '   <md-button  '  + 
+ '       ng-class="{'active' : isSelected()}"  '  + 
+ '       ui-sref="{{section.state}}"  '  + 
+ '       ng-click="focusSection()">  '  + 
+ '     {{section.name}}  '  + 
+ '     <span class="md-visually-hidden"  '  + 
+ '       ng-if="isSelected()">  '  + 
+ '       current page  '  + 
+ '     </span>  '  + 
+ '  </md-button>  ' ;   
+   
+  return {    
+    restrict: 'E',
+    template: htmlTemplate,
+    link: function(scope, element) {
+   	
+      scope.isSelected = function() {
+        return controller.isSelected(scope.section);
+      };
+
+      scope.focusSection = function() {
+        // set flag to be used later when
+        // $locationChangeSuccess calls openPage()
+        controller.autoFocusContent = true;
+      };
+    }
+  };
+})
 	
 
 
